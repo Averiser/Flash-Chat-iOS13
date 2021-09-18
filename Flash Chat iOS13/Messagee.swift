f46159506e263c350583b9277a86c3b6f57aa98c
Averiser <averiser@gmail.com>
2021-09-18 12:26:21 +0300

    sixth commit - UITableViewDataSource and Message Model

---
+++ Flash Chat iOS13/Messagee.swift
@@ -0,0 +1,14 @@
+//
+//  Messagee.swift
+//  Flash Chat iOS13
+//
+//  Created by MyMacBook on 18.09.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+  let sender: String
+  let body: String
+}
